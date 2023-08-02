f7345b545639b453538492768be23f96f3a7116e
ym733 <ym7330694@gmail.com>
2023-08-03 02:00:09 +0300

    viewmodel moved to data folder

---
+++ Data/ViewModel/CategoryType.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viewModel
+{
+    public class CategoryType
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+        public IFormFile files { get; set; }
+    }
+}
