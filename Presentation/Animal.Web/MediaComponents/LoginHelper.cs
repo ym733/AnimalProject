fea21a86ec80c029f5c76c3dca0f9190ffd03a85
ym733 <ym7330694@gmail.com>
2023-08-14 17:17:20 +0300

    created custom HTML helpers turned the entire login page to html helpers to-do: turn everything in all form to html helpers

---
+++ Presentation/Animal.Web/MediaComponents/LoginHelper.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Animal.Web.MediaComponents
+{
+	public static class LoginHelper
+	{
+		public static IHtmlContent TextInput<TModel, TProperty>(this IHtmlHelper<TModel> helper, Expression<Func<TModel, TProperty>> expression)
+		{
+			var builder = new StringBuilder();
+			builder.Append(helper.ValidationMessageFor(expression, null, new { @class = "d-block" }).GetHtmlContentString());
+
+			var div1 = new TagBuilder("div");
+			div1.InnerHtml.Append(helper.LabelFor(expression).GetHtmlContentString());
+			div1.InnerHtml.Append(": ");
+			builder.Append(div1.GetHtmlContentString());
+
+			var div2 = new TagBuilder("div");
+			div2.InnerHtml.Append(helper.TextBoxFor(expression).GetHtmlContentString());
+			builder.Append(div2.GetHtmlContentString());
+
+			return new HtmlString(builder.ToString());
+		}
+
+		public static IHtmlContent PasswordInput<TModel, TProperty>(this IHtmlHelper<TModel> helper, Expression<Func<TModel, TProperty>> expression)
+		{
+			var builder = new StringBuilder();
+			builder.Append(helper.ValidationMessageFor(expression, null, new { @class = "d-block" }).GetHtmlContentString());
+
+			var div1 = new TagBuilder("div");
+			div1.InnerHtml.Append(helper.LabelFor(expression).GetHtmlContentString());
+			div1.InnerHtml.Append(": ");
+			builder.Append(div1.GetHtmlContentString());
+
+			builder.Append(helper.PasswordFor(expression, new { @class = "d-block" }).GetHtmlContentString());
+			
+
+			return new HtmlString(builder.ToString());
+		}
+		public static IHtmlContent SubmitButton<TModel>(this IHtmlHelper<TModel> helper, string buttonText)
+		{
+			var builder = new StringBuilder();
+			var div = new TagBuilder("div");
+			builder.Append(div.GetHtmlContentString());
+
+			var button = new TagBuilder("input");
+			button.Attributes.Add("type", "submit");
+			button.Attributes.Add("value", buttonText);
+
+			builder.Append(button.GetHtmlContentString());
+
+			return new HtmlString(builder.ToString());
+		}
+	}
+}
---
+++ Presentation/Animal.Web/MediaComponents/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Html;
+using System.Runtime.CompilerServices;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace Animal.Web.MediaComponents
+{
+	public static class StringExtensions
+	{
+		public static string GetHtmlContentString(this IHtmlContent content)
+		{
+			using (var writer = new StringWriter())
+			{
+				content.WriteTo(writer, HtmlEncoder.Create(new[] { UnicodeRanges.BasicLatin }));
+				return System.Net.WebUtility.HtmlDecode(writer.ToString());
+			}
+		}
+	}
+}
