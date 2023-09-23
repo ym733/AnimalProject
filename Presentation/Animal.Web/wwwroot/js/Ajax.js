4484c219b49790cb4822f2f0137022de921aed81
ym733 <ym7330694@gmail.com>
2023-09-23 22:44:02 +0300

    made all ajax methods in one file

---
+++ Presentation/Animal.Web/wwwroot/js/Ajax.js
@@ -0,0 +1,64 @@
+﻿let divNum = 1;
+
+function changeMain(url) {
+	if (url === window.location.pathname) {
+		return;
+	}
+
+	history.pushState({ url, divNum }, null, url, divNum);
+	ajaxCall(url);
+}
+
+window.addEventListener('popstate', function (event) {
+	if (event.state) {
+		if (divNum > event.state.divNum + 1) {
+			ajaxBackCall();
+		} else {
+			const url = event.state.url;
+			ajaxCall(url);
+		}
+	} else {
+		ajaxBackCall();
+	}
+});
+
+function ajaxBackCall() {
+	$(`#level${divNum}`).remove();
+	divNum--;
+	$(`#level${divNum}`).removeAttr(`hidden`);
+}
+
+function ajaxCall(url) {
+	$.ajax({
+		url: url,
+		method: "GET",
+		dataType: "html",
+		success: (data) => {
+			$(`#level${divNum}`).attr("hidden", "hidden")
+			divNum++;
+			$(`main`).append(`<div id="level${divNum}">${data}</div>`)
+		},
+		error: (err) => {
+			console.error(err);
+		}
+	});
+}
+function formSubmit(url, form, event) {
+	event.preventDefault();
+
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: new FormData(form),
+		contentType: false,
+		processData: false,
+		success: function (data) {
+			$(`#level${divNum}`).attr("hidden", "hidden")
+			divNum++;
+			$(`main`).append(`<div id="level${divNum}">${data}</div>`)
+		},
+		error: function (err) {
+			console.error(err);
+		}
+	});
+}
